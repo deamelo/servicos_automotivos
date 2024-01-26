bd5ffd2675a13e3dffdacd171fbaae5846880b33
dea <deyahmelo@hotmail.com>
2024-01-26 12:16:34 -0300

    Form de cliente - atualizar cliente

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.clientes, name='clientes'),
-    path('atualiza_cliente', views.atualiza_cliente, name='atualiza_cliente'),
+    path('', views.clientes, name="clientes"),
+    path('atualiza_cliente/', views.atualiza_cliente, name="atualiza_cliente"),
+
 ]
