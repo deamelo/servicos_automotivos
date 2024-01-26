4c48dc9dec3a810de13a376ef8809182c30b2ac9
dea <deyahmelo@hotmail.com>
2024-01-26 19:32:36 -0300

    atualiza e exclui carro - atualizar cliente

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
+from django.shortcuts import redirect
 import re
 import json
 
@@ -42,10 +45,47 @@ def clientes(request):
             carro = Carro(marca=marca, modelo=modelo, placa=placa, cliente=cliente)
             carro.save()
 
-        return HttpResponse('OI')
+        return HttpResponse(carro)
 
 def atualiza_cliente(request):
-    id_cliente = request.POST.get('id_cliente')
-    cliente = Cliente.objects.filter(id=id_cliente)
+    get_cliente = request.POST.get('id_cliente')
+    cliente = Cliente.objects.filter(id=get_cliente)
+    carros = Carro.objects.filter(cliente=cliente[0])
+
+    cliente_id = json.loads(serializers.serialize('json', cliente))[0]['pk']
     cliente_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
-    return JsonResponse(cliente_json)
+    carros_json = json.loads(serializers.serialize('json', carros))
+    carros_json = [{'fields': carro['fields'], 'id': carro['pk']} for carro in carros_json]
+
+    data = {'cliente': cliente_json, 'carros': carros_json, 'cliente_id': cliente_id}
+
+
+    return JsonResponse(data)
+
+@csrf_exempt
+def atualiza_carro(request, id):
+    marca = request.POST.get('marca')
+    modelo = request.POST.get('modelo')
+    placa = request.POST.get('placa')
+
+    carro = Carro.objects.get(id=id)
+    placa_carro = Carro.objects.filter(placa=placa).exclude(id=id)
+    print(placa_carro)
+    if placa_carro.exists():
+        return HttpResponse('Placa já existe')
+
+    carro.marca = marca
+    carro.modelo = modelo
+    carro.placa = placa
+    carro.save()
+    return HttpResponse('Dados alterados com sucesso')
+
+def excluir_carro(request, id):
+    try:
+        carro = Carro.objects.get(id=id)
+        carro.delete()
+        return redirect(reverse('clientes')+f'?aba=atualiza_cliente&id_cliente={id}')
+    except Carro.DoesNotExist:
+        return redirect(reverse('clientes')+f'?aba=atualiza_cliente&id_cliente={id}')
+
+
