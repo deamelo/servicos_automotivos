824fb3319fdc4fcd12fb47a9ab32984fca82a55c
dea <deyahmelo@hotmail.com>
2024-01-29 01:52:56 -0300

    listar servicos

--- servicos/models.py
+++ servicos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from clientes.models import Cliente
 from .choices import ChoicesCategoriaManutenção
 from datetime import datetime
-from secrets import token_hex
+from secrets import token_hex, token_urlsafe
 
 class CategoriaManutencao(models.Model):
     titulo = models.CharField(max_length=3, choices=ChoicesCategoriaManutenção.choices)
@@ -19,6 +19,7 @@ class Servico(models.Model):
     data_entrega = models.DateField(null=True)
     finalizado = models.BooleanField(default=False)
     protocolo = models.CharField(max_length=44, null=True, blank=True)
+    identificador = models.CharField(max_length=24, null=True, blank=True)
 
     def __str__(self):
         return self.titulo
@@ -27,7 +28,12 @@ class Servico(models.Model):
     def save(self, *args, **kwargs):
         if not self.protocolo:
             self.protocolo = datetime.now().strftime('%d-%m-%Y-%H:%M:%S-') + token_hex(12)
+
+        if not self.identificador:
+            self.identificador = token_urlsafe(16)
+
         super(Servico,self).save(*args, **kwargs)
+
 
     def preco_total(self):
         preco_total = float(0)
